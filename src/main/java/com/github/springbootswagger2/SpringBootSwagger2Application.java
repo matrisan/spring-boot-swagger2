249c362a2ab1f7ca4d8f5d46482fdc0875d2ebe1
shishaodong <shishaodong@cmhi.chinamobile.com>
2019-04-14 10:52:40 +0800

    Initial commit

---
+++ src/main/java/com/github/springbootswagger2/SpringBootSwagger2Application.java
@@ -0,0 +1,16 @@
+package com.github.springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shao
+ */
+@SpringBootApplication
+public class SpringBootSwagger2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwagger2Application.class, args);
+    }
+
+}
